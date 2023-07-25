27d13c6fdb35ce6cf5a3e8d8b2d52bb0e24d1ceb
alexm <alex-m@outlook.com>
2023-07-25 16:17:21 -0700

    Create Card component definition and documentation

---
+++ src/Card.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+/**
+ * Card component - display an image of a single card.
+ *
+ * Props:
+ * - image (string): a URL to the card's image
+ *
+ * State: none
+ * Events: none
+ */
+function Card ({image}) {
+
+}
+
+
+export default Card;
