06a387f9fca627843a68bc2cac047f1d596a35c9
alexm <alex-m@outlook.com>
2023-07-25 16:19:14 -0700

    Define Deck component and render its title; add to docstring

--- src/Deck.js
+++ src/Deck.js
@@ -1,4 +1,10 @@
+import React, {useState} from "react";
+import Card from "./Card";
+
 /**
+ * Deck component - display cards drawn from a standard 52-card deck and buttons for drawing cards
+ * and reshuffling the deck.
+ *
  * Props:
  * - title (string): custom title for this deck
  * - id (string): the ID of this deck
@@ -12,3 +18,15 @@
  * - Click (button to draw new card): draw a new card from this deck
  * - Click (button to reshuffle deck): shuffle this deck
  */
+function Deck({id, title="Deck of Cards"}) {
+
+
+    return (
+        <div className="Deck">
+            <h1>{title}</h1>
+        </div>
+    )
+}
+
+
+export default Deck;
