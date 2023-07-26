daae5326e94c802807a4ff229723cbac77d63e44
alexm <alex-m@outlook.com>
2023-07-26 00:05:29 -0700

    Add dummy test URL and snapshot test

--- src/Card.test.js
+++ src/Card.test.js
@@ -3,6 +3,15 @@ import { render } from '@testing-library/react';
 import Card from './Card';
 
 
+const TEST_URL = "test.png";
+
+
 test("Renders without crashing", () => {
-    render(<Card />);
+    render(<Card imageUrl={TEST_URL} />);
+});
+
+
+test("Matches snapshot", () => {
+    const {asFragment} = render(<Card imageUrl={TEST_URL} />);
+    expect(asFragment()).toMatchSnapshot();
 });
