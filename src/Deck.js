1ac3c5c2f676cf988427f27d90538c88d0ec2628
alexm <alex-m@outlook.com>
2023-07-25 17:43:04 -0700

    Update docstring with new piece of state; add code to retrieve new deck from API, set deck state and display deck ID

--- src/Deck.js
+++ src/Deck.js
@@ -1,5 +1,7 @@
-import React, {useState} from "react";
+import React, {useState, useEffect} from "react";
+import axios from "axios";
 import Card from "./Card";
+
 
 /**
  * Deck component - display cards drawn from a standard 52-card deck and buttons for drawing cards
@@ -7,23 +9,48 @@ import Card from "./Card";
  *
  * Props:
  * - title (string): custom title for this deck
- * - id (string): the ID of this deck
  *
  * State:
- * - cards (array): an array holding all cards currently in the deck.
+ * - deck (object): holds info about this deck.
+ *      - Deck state fields: {deck_id, shuffled, remaining}
+ * - drawnCards (array): an array holding all cards currently drawn from the deck.
  *      - Each card is represented by an object holding its info:
  *          {code, image, images, value, suit}
- *
  * Events:
  * - Click (button to draw new card): draw a new card from this deck
  * - Click (button to reshuffle deck): shuffle this deck
  */
-function Deck({id, title="Deck of Cards"}) {
+function Deck({title="Deck of Cards"}) {
+    const [deck, setDeck] = useState(null);
+    const [drawnCards, setDrawnCards] = useState([]);
 
+    const baseUrl = "https://deckofcardsapi.com/api/deck";
 
+    // console.log("Before useEffect call");
+
+    // Get a fresh deck from API
+    useEffect(() => {
+        // console.log("In useEffect()");
+        async function fetchDeck() {
+            // console.log("Inside fetchDeck() - beginning");
+            const deckResult = await axios.get(`${baseUrl}/new/shuffle`);
+            const {deck_id, remaining, shuffled} = deckResult.data;
+            setDeck({deck_id, remaining, shuffled});
+            // console.log("End of fetchDeck()");
+        }
+
+        // console.log("Before call to fetchDeck()");
+        fetchDeck();
+        // console.log("After call to fetchDeck()");
+    }, []);
+
+    // console.log("After useEffect() call");
     return (
         <div className="Deck">
             <h1>{title}</h1>
+            <div>
+                {deck ? <h2>Deck ID: {deck.deck_id}</h2> : <h2>Loading...</h2>}
+            </div>
         </div>
     )
 }
